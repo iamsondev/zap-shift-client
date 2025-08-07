b1d7fa0f4c4deaccfa7648d32d55e325cd3453ce
iamsondev <sondipkumarsk@gmail.com>
2025-08-07 14:21:58 +0600

    added context and firebase Authentication

--- src/Pages/Authentication/Registration/Register.jsx
+++ src/Pages/Authentication/Registration/Register.jsx
@@ -20,14 +20,14 @@ const Register = () => {
                 <label className="label">Email</label>
                 <input type="email" {...register('email', {required:true})} className="input" placeholder="Email" />
                 {
-                  errors.email && <p className='text-red-500'>Email is required</p>
+                  errors.email?.type==='required' && <p className='text-red-500'>Email is required</p>
                 }
 
                    {/* password */}
                 <label className="label">Password</label>
                 <input type="password" {...register('password', {required:true , minLength:7} )} className="input" placeholder="Password" />
                 {
-                  errors.password && <p className='text-red-500'>Password is required</p>
+                  errors.password?.type==='required' && <p className='text-red-500'>Password is required</p>
                 }
                 {
                   errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be at least 6 characters</p>
